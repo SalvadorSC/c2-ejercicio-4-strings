e6f491a79551715c1ebf3f6219bef3f3eff40c6f
Salvador Sánchez <sanchezcampossalvador@gmail.com>
2021-05-28 11:01:15 +0200

    corregido ejercicio

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 const palabras = ["café", "croissant", "tostada", "zumo"];
-/* let palabrasParesModificadas = modificaPares(
-  palabras,
-  (elemento) => elemento.charAt(0).toUpperCase() + elemento.slice(1)
-); */
-function modificaPares(
-  arrayPalabras,
-  modificaPares = (palabras,
-  (elemento) => elemento[0].toUpperCase() + elemento.slice(1))
-) {
-  const palabrasModificadas = [];
 
-  for (i in arrayPalabras) {
-    i % 2 !== 0
-      ? palabrasModificadas.push(
-          modificaPares(
-            arrayPalabras[i],
-            (palabra) =>
-              palabra[0].charAt(0).toUpperCase() + palabra[i].slice(1)
-          )
-        )
-      : palabrasModificadas.push(arrayPalabras[i]);
+function modificaPares(arrayPalabras, callback) {
+  const palabrasModificadas = [];
+  for (const i in arrayPalabras) {
+    if (i % 2 !== 0) {
+      palabrasModificadas.push(callback(arrayPalabras[i]));
+    } else palabrasModificadas.push(arrayPalabras[i]);
   }
   console.log(palabrasModificadas);
   return palabrasModificadas;
 }
 
-modificaPares(palabras);
+modificaPares(
+  palabras,
+  (palabra) => palabra[0].charAt(0).toUpperCase() + palabra.slice(1)
+);
